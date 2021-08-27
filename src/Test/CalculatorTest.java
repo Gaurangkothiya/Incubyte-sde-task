d7f154d3d692e96549dee6c46b17678fa5f34a71
Gaurangkothiya <gaurangkothiya1234@gmail.com>
2021-08-27 17:59:15 +0530

    Added Testcase for emptystring

---
+++ src/Test/CalculatorTest.java
@@ -0,0 +1,28 @@
+package Test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import main.Calculator;
+
+public class CalculatorTest {
+	
+	Calculator cal;
+
+@Before
+public void setUp() {
+    cal = new Calculator();
+}	
+	
+	
+@Test
+ public  void testCreate()
+ {
+ }
+
+@Test
+public void testEmptyString() {
+	assertEquals(0,cal.Add(""));
+}
+
+}
---
+++ src/main/Calculator.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Calculator {
+	public Calculator()
+	{
+	}
+	
+	public int Add(String s) {
+		return 0;
+	}
+}
